8739739b521be2747bc922afdbce862a5c3c5608
TimeWz667 <TimeWz667@gmail.com>
2023-01-10 15:36:02 +0000

    add example data to app

---
+++ app/src/.vuepress/components/dataLoader.vue
@@ -0,0 +1,42 @@
+<template>
+<div>
+  {{CurrentData}}
+  Test
+</div>
+</template>
+
+<script>
+import pars_all from "../public/pars/pars_India.json";
+import * as d3 from "d3";
+
+const Locations = ["India", "Delhi", "Bihar", "Andhra_Pradesh"];
+
+export default {
+  name: "data_loader",
+  data() {
+    return {
+      CurrentData: pars_all
+    }
+  },
+  mounted() {
+    console.log("mounted");
+    this.fetchData(this.Location);
+  },
+  props: {
+    Location: {
+      type: String,
+      default: "India"
+    }
+  },
+  methods: {
+    fetchData(loc) {
+      console.log(loc)
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- app/src/cascade/index.md
+++ app/src/cascade/index.md
@@ -1,2 +1,4 @@
 ## Cascade
 
+<dataLoader></dataLoader>
+

24c846ddaf216035c9f040c84df3e6274ad548f8
TimeWz667 <TimeWz667@gmail.com>
2023-04-04 00:55:38 +0100

    Add vis

---
+++ scripts/3_vis_posterior.R
@@ -0,0 +1,70 @@
+library(tidyverse)
+library(tidybayes)
+
+
+theme_set(theme_bw())
+
+
+
+
+post <- read_csv(here::here("out", "cs1ppv1dur1sub1", "post.csv"))
+
+
+
+
+
+g_ppm <- post %>% 
+  ggplot() + 
+  stat_halfeye(aes(x = ppm)) + 
+  scale_x_continuous("Engaged private visits / all private visits, %", labels = scales::percent_format()) +
+  scale_y_continuous("") + 
+  expand_limits(x = c(0, 1)) +
+  theme(axis.text.y = element_blank())
+
+
+
+g_pdx_pri <- post %>% 
+  ggplot() + 
+  stat_halfeye(aes(x = pdx_eng)) + 
+  scale_x_continuous("TB case detected per private visits, %", labels = scales::percent_format()) +
+  scale_y_continuous("") + 
+  expand_limits(x = c(0, 1)) +
+  theme(axis.text.y = element_blank())
+
+
+
+
+g_dur_pri <- post %>% 
+  ggplot() + 
+  stat_halfeye(aes(x = dur_pri)) + 
+  scale_x_continuous("Treatment duration, on private drug, month", labels = scales::number_format(scale = 12)) +
+  scale_y_continuous("") + 
+  expand_limits(x = 0) +
+  theme(axis.text.y = element_blank())
+
+
+g_dur_pri
+
+g_ppv_pri <- post %>% 
+  ggplot() + 
+  stat_halfeye(aes(x = ppv_pri)) + 
+  scale_x_continuous("Positive Predictive Value, in the private sector, %", labels = scales::percent_format()) +
+  scale_y_continuous("") + 
+  expand_limits(x = c(0, 1)) +
+  theme(axis.text.y = element_blank())
+
+
+g_bind <- ggpubr::ggarrange(g_ppm, g_pdx_pri, g_ppv_pri, g_dur_pri, ncol=1)
+
+
+
+ggsave(g_bind, filename = here::here("out", "cs1ppv1dur1sub1", "g_posterior.png"), width = 5, height = 5)
+
+
+
+
+
+
+
+
+

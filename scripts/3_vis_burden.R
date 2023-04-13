2343137264da52195546953bc4617d1950d44014
TimeWz667 <TimeWz667@gmail.com>
2023-04-13 18:47:57 +0100

    Add vis scripts

--- scripts/3_vis_burden.R
+++ scripts/3_vis_burden.R
@@ -12,12 +12,21 @@ post <- bind_rows(lapply(c("tx_00", "tx_01", "tx_11"), function(folder) {
 }))
 
 
-post %>% 
+g_attload <- post %>% 
   extract(Scenario, c("TBPS", "DrugSale"), "_(\\d)(\\d)", remove=F) %>% 
   pivot_longer(starts_with("tp_pri")) %>% 
   ggplot() +
   stat_halfeye(aes(x = value, y = name, fill = Scenario), position = position_dodge(-0.2), alpha=0.4) +
   #facet_grid(TBPS~DrugSale, labeller = label_both) +
-  scale_x_log10("Caseloads, millions", labels=scales::number_format(scale = 1e-6))
+  scale_x_continuous("Caseloads, millions", labels=scales::number_format(scale = 1e-6), limits = c(0, 5e6)) +
+  scale_y_discrete("", labels=c(
+    tp_pri_drug = "Txi with private drugs",
+    tp_pri_drug_time = "Caseloads with private drugs",
+    tp_pri_txi = "Txi in the unengaged private"
+  )) +
+  scale_fill_discrete("Data sets", labels=c(tx_00="Nikshay only", tx_01="+Drug sales", tx_11="+TBPS"))
+
+ggsave(g_attload, filename=here::here("doc"))
+
 
 
--- scripts/3_vis_posterior.R
+++ scripts/3_vis_posterior.R
@@ -4,14 +4,12 @@ library(tidybayes)
 theme_set(theme_bw() + theme(text = element_text(family = "sans")))
 
 
-
-
 post <- read_csv(here::here("out", "cs_11", "post.csv"))
 
 
 g_ppm <- post %>% 
   ggplot() + 
-  stat_halfeye(aes(x = ppm)) + 
+  stat_halfeye(aes(x = ppm), alpha = 0.7) + 
   scale_x_continuous("Engaged private visits / all private visits, %", labels = scales::percent_format()) +
   scale_y_continuous("") + 
   expand_limits(x = c(0, 1)) +
@@ -21,7 +19,7 @@ g_ppm
 
 g_pdx_pri <- post %>% 
   ggplot() + 
-  stat_halfeye(aes(x = pdx_eng)) + 
+  stat_halfeye(aes(x = pdx_eng), alpha = 0.7) + 
   scale_x_continuous("TB case detected per private visits, %", labels = scales::percent_format()) +
   scale_y_continuous("") + 
   expand_limits(x = c(0, 1)) +
@@ -32,7 +30,7 @@ g_pdx_pri
 
 g_dur_pri <- post %>% 
   ggplot() + 
-  stat_halfeye(aes(x = dur_pri)) + 
+  stat_halfeye(aes(x = dur_pri), alpha = 0.7) + 
   scale_x_continuous("Treatment duration, on private drug, month", labels = scales::number_format(scale = 12)) +
   scale_y_continuous("") + 
   expand_limits(x = 0) +
@@ -43,7 +41,7 @@ g_dur_pri
 
 g_ppv_pri <- post %>% 
   ggplot() + 
-  stat_halfeye(aes(x = ppv_pri)) + 
+  stat_halfeye(aes(x = ppv_pri), alpha = 0.7) + 
   scale_x_continuous("Positive Predictive Value, in the private sector, %", labels = scales::percent_format()) +
   scale_y_continuous("") + 
   expand_limits(x = c(0, 1)) +

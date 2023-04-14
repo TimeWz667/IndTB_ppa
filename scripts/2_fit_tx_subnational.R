1198296063cbfaad4c80dbb16375b1b32b1f2376
TimeWz667 <TimeWz667@gmail.com>
2023-04-14 11:11:58 +0100

    Add subnational

--- scripts/2_fit_tx_subnational.R
+++ scripts/2_fit_tx_subnational.R
@@ -7,28 +7,59 @@ rstan_options(auto_write = TRUE)
 
 ## Data loading
 
-targets <- read_csv(here::here("data", "targets_india.csv"))
+pop <- local({
+  locs <- dir("data")
+  locs <- locs[startsWith(locs, "targets_")]
+  locs <- gsub("targets_", "", locs)
+  locs <- gsub(".csv", "", locs)
+  locs <- locs[locs != "india"]
+  locs
+  
+  
+  pop <- read_csv(here::here("data", "population.csv"))
+  
+  pop <- pop %>% 
+    filter(Year == 2019) %>% 
+    filter(Sex == "Total") %>% 
+    mutate(
+      Location = gsub("\\s+", "_", Location),
+      Location = gsub("&", "and", Location)
+    )
+  
+  pop <- set_names(pop$Pop, pop$Location)[locs]
+  
+  pop
+})
 
-drug <- targets %>% filter(Index == "DrugTime")
-tx <- targets %>% filter(Index == "PrTxiPub") %>% 
-  mutate(X = round(M * N))
-
-det <- targets %>% 
-  filter(Year == 2019) %>% 
-  filter(Index == "TxI") %>% 
-  mutate(N_Txi = round(N * M))
 
 
-ds <- list(
-  N_Txi_Pub = det$N_Txi[1],
-  N_Txi_Eng = det$N_Txi[2],
-  Pop = 1383112050,
-  Tx = tx$N,
-  Tx_Pub = tx$X,
-  Drug = drug$M,
-  Drug_Std = drug$Error,
-  ppv_pub = 0.85
-)
+data_locs <- lapply(names(pop), function(loc) {
+  
+  targets <- read_csv(here::here("data", "targets_" + glue::as_glue(loc) + ".csv"))
+  
+  drug <- targets %>% filter(Index == "DrugTime")
+  tx <- targets %>% filter(Index == "PrTxiPub") %>% 
+    mutate(X = round(M * N))
+  
+  det <- targets %>% 
+    filter(Year == 2019) %>% 
+    filter(Index == "TxI") %>% 
+    mutate(N_Txi = round(N * M))
+  
+  
+  ds <- list(
+    N_Txi_Pub = det$N_Txi[1],
+    N_Txi_Eng = det$N_Txi[2],
+    Pop = unname(pop[loc]),
+    Tx = tx$N,
+    Tx_Pub = tx$X,
+    Drug = drug$M,
+    Drug_Std = drug$Error,
+    ppv_pub = 0.85
+  )
+  
+})
+names(data_locs) <- names(pop)
 
 
 
@@ -37,16 +68,14 @@ ds <- list(
 src_model <- "tx_11.stan"
 model <- rstan::stan_model(here::here("stan", src_model))
 
-for(loc in locs) {
-  
-  
-  
-  
-}
+folder <- "tx_11_subnational"
+dir.create(here::here("out", folder), showWarnings = F)
 
-
-for(src_model in c("tx_00.stan", "tx_10.stan", "tx_01.stan", "tx_11.stan")) {
-
+for(loc in names(data_locs)) {
+  print(loc)
+  
+  ds <- data_locs[[loc]]
+  
   
   post <- rstan::sampling(model, data=ds, iter=5e3, warmup=4e3)
   
@@ -63,13 +92,7 @@ for(src_model in c("tx_00.stan", "tx_10.stan", "tx_01.stan", "tx_11.stan")) {
       pdx_pub = ds$pdx_pub
     )
   
-  folder <- gsub(".stan", "", src_model)
-  dir.create(here::here("out", folder), showWarnings = F)
-  write_csv(tab, file = here::here("out", folder, "summary.csv"))
-  write_csv(ext, file = here::here("out", folder, "post.csv"))
+  write_csv(tab, file = here::here("out", folder, "summary_" + glue::as_glue(loc) + ".csv"))
+  write_csv(ext, file = here::here("out", folder, "post_" + glue::as_glue(loc) + ".csv"))
 }
-
-
-
-
 
